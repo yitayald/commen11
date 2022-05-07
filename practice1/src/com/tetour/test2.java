8c7f66353a19d9017c023e671153be6e1f7f5c0f
yitayald <yitayaldebalke@gmail.com>
2022-05-06 23:00:07 -0400

    commit from eclips

---
+++ practice1/src/com/tetour/test2.java
@@ -0,0 +1,13 @@
+package com.tetour;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		System.out.println("welcome to batch 26");
+		System.out.println("hello world welcome");
+		System.out.println("welcome to batch 26 in april");
+	}
+	
+}
